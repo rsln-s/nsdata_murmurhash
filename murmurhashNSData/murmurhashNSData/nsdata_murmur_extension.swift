79623404e9947d3bb4fbbebeb0c59764d2e60294
Ruslan Shaydulin <ruslan@l2sh.ru>
2015-08-24 18:40:21 +0300

    Revert "Revert "func is back, "get {" is out""
    This reverts commit a81d0e1640988f2dae1600bb358c98b36d3b2bbb.

--- murmurhashNSData/murmurhashNSData/nsdata_murmur_extension.swift
+++ murmurhashNSData/murmurhashNSData/nsdata_murmur_extension.swift
@@ -11,10 +11,12 @@ import Foundation
 extension NSData {
     
     var MurMurHash : UInt32 {
-        get {
-                let data = mydata.bytes
-                let size = mydata.length
-                return murmurhash(UnsafePointer<Int8>(data), UInt32(size), UInt32(0))
-        }
+        return self.MurMurHash(UInt32(0))
+    }
+    
+    func MurMurHash (seed: UInt32) -> UInt32{
+        let data = mydata.bytes
+        let size = mydata.length
+        return murmurhash(UnsafePointer<Int8>(data), UInt32(size), seed)
     }
 }
